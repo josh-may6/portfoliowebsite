84cdd26ecce03b11c5ef66c648ac1a45ea7d0411
Josh Maitre <joshmaitre6@gmail.com>
2023-12-11 14:28:53 -0800

    Add Header.html & Footer.html

--- app.py
+++ app.py
@@ -55,5 +55,10 @@ def receive_data():
     return redirect(url_for('contact'))
 
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
